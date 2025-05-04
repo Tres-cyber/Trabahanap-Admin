27ef602f657b22a521bd308325f562cc75cacd20
Tres-cyber <louiegalamaythe3rd@gmail.com>
2025-05-04 11:56:47 +0800

    Added Users to Route

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import { LoginPage } from './pages/login/LoginPage';
 import { HomePage } from './pages/home/HomePage';
 import UsersPage from './pages/users';
+import UserProfilePage from './pages/users/[id]';
+import VerificationPage from './pages/verification';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/users" element={<UsersPage />} />
+        <Route path="/users/:id" element={<UserProfilePage />} />
+        <Route path="/verification" element={<VerificationPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
